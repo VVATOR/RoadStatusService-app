c09c6589bf9952eb4d379e309e10caf5cbc272f2
Vitali Vikhliayeu <Vitali_Vikhliayeu@epam.com>
2018-01-12 09:16:53 +0300

    map + send photo

---
+++ app/src/main/java/by/gsu/roadstatusservice_app/PhotoActivity.java
@@ -0,0 +1,116 @@
+package by.gsu.roadstatusservice_app;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+
+import by.gsu.RoadStatusService.models.Picture;
+import by.gsu.RoadStatusService.models.Point;
+import by.gsu.client.Client;
+import by.gsu.client.IRoadStatusClient;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    private IRoadStatusClient client = new Client();
+    private ImageView iv;
+    private Button buttonSave;
+    private EditText editFileName;
+    private EditText editDescription;
+    private Location location;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, sb.toString(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        iv = (ImageView) findViewById(R.id.photoImageView);
+        editFileName = (EditText) findViewById(R.id.editFileName);
+        editDescription = (EditText) findViewById(R.id.editDescription);
+
+        Intent data = getIntent();
+
+        //String fName = intent.getStringExtra("fname");
+
+
+        location = (Location) getIntent().getParcelableExtra(Location.class.getCanonicalName());
+        ;
+
+
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        iv.setImageBitmap(bitmap);
+
+
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                BitmapDrawable drawable = (BitmapDrawable) iv.getDrawable();
+
+                Bitmap bitmap1 = drawable.getBitmap();
+                bitmap1.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+                String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+                Point point = new Point(location.getLatitude(), location.getLongitude());
+
+                String fname=editFileName.getText().equals("")?System.currentTimeMillis() + ".jpg":editFileName.getText() + ".jpg";
+                String escription=""+ editDescription.getText();
+                newPicture = new Picture(331, fname, escription, point, encoded);
+
+                new AsyncTask<Integer, Integer, String>() {
+                    @Override
+                    protected String doInBackground(Integer... arg) {
+                        client.methodPostPicture(newPicture);
+                        return "";
+                    }
+
+                    @Override
+                    protected void onPostExecute(String s) {
+                        super.onPostExecute(s);
+                        sb.append("lol");
+                    }
+                }.execute();
+
+
+            }
+        });
+
+    }
+
+    private Picture newPicture;
+    private StringBuilder sb = new StringBuilder("Replace with your own action");
+
+
+}
+
+
+
---
+++ app/src/main/java/by/gsu/roadstatusservice_app/utils/LocationUtils.java
@@ -0,0 +1,56 @@
+package by.gsu.roadstatusservice_app.utils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import by.gsu.roadstatusservice_app.exceptions.LocationException;
+
+/**
+ * Created by Vitali_Vikhliayeu on 2018-01-04.
+ */
+
+public class LocationUtils implements LocationListener {
+    private Activity activity;
+
+    public LocationUtils(Activity activity) {
+        this.activity = activity;
+    }
+
+    public Location getLocation() throws LocationException {
+        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            throw new LocationException();
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //  txtLat = (TextView) findViewById(R.id.txtLocationText);
+        //  txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Provider", "disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Provider", "enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Provider", "status:" + status);
+    }
+}
